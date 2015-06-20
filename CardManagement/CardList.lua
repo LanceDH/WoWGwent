7a3be056b4068e490bd52fc092ca0d842216dc8e
LanceDH <kiep_out@hotmail.com>
2015-06-20 17:21:09 +0200

    Cqrdlist test & improved debug

---
+++ CardManagement/CardList.lua
@@ -0,0 +1,53 @@
+﻿local addonName, GwentAddon = ...
+
+GwentAddon.CardList = {}
+
+local DECK_NORTH = "Northern Realms"
+
+local ABILITY_Spy = "Spy"
+local ABILITY_Bond =  "Tight Bond"
+
+function GwentAddon:CreateCardsList()
+
+	GwentAddon.CardList = {}
+
+	table.insert(GwentAddon.CardList, {
+				name = "Thaler"
+				,deck = DECK_NORTH
+				,strength = 1
+				,cardType = {melee = false, ranged = false, siege = true, hero = false}
+				,ability = ABILITY_Spy
+			})
+			
+	table.insert(GwentAddon.CardList, {
+				name = "Redanian Foot Soldier"
+				,deck = DECK_NORTH
+				,strength = 1
+				,cardType = {melee = true, ranged = false, siege = false, hero = false}
+				,ability = nil
+			})
+			
+	table.insert(GwentAddon.CardList, {
+				name = "Poor Fucking Infantry"
+				,deck = DECK_NORTH
+				,strength = 1
+				,cardType = {melee = true, ranged = false, siege = false, hero = false}
+				,ability = ABILITY_Bond
+			})
+		
+
+	for k, v in ipairs(GwentAddon.CardList) do
+		v.Id = k
+		print(v.name .. " got Id " .. v.Id)
+
+	end
+
+	print("Card List Created")
+
+	
+end
+
+
+
+
+
--- Gwent.lua
+++ Gwent.lua
@@ -1,7 +1,8 @@
-﻿local addonName, BGS_TrackerClasses = ...
+﻿local addonName, GwentAddon = ...
 local AceGUI = LibStub("AceGUI-3.0")
 
-local DEBUGWINDOW = nil
+local DEBUGWINDOW = {}
+local DEBUGMESSAGES = {}
 local TEXT_ADDONMSG_RECIEVED = "Message Recieved"
 
 
@@ -18,91 +19,178 @@ local function round(num, idp)
 	return ret
 end
 
+local function DEBUGPrintCard(card)
+	local messageSender = AceGUI:Create("Label")
+	messageSender:SetRelativeWidth(1)
+	messageSender:SetText("|cFFE6D707"..card.Id .. ": " .. card.name.. "|r")
+	DEBUGWINDOW.cardScroller:AddChild(messageSender)
+	
+	local messageText = AceGUI:Create("Label")
+	messageText:SetRelativeWidth(1)
+	messageText:SetText("> Str: "..card.strength .. " deck: "..card.deck)
+	DEBUGWINDOW.cardScroller:AddChild(messageText)
+	-- local messageSender = AceGUI:Create("SimpleGroup")
+	-- messageSender:SetRelativeWidth(1)
+	-- messageSender:SetHeight(10)
+	-- messageSender:SetLayout("fill")
+	-- messageSender.text = messageSender.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
+	-- messageSender.text:SetText("|cFFE6D707"..card.Id .. ": " .. card.name.. "|r")
+	-- messageSender.text:SetJustifyH("left")
+	-- messageSender.text:SetJustifyV("top")
+	-- messageSender.text:SetPoint("topleft", 0, 0)
+	-- messageSender.text:SetPoint("bottomright")
+	-- DEBUGWINDOW.cardScroller:AddChild(messageSender)
+	
+	
+	
+	-- local messageText = AceGUI:Create("SimpleGroup")
+	-- messageText:SetRelativeWidth(1)
+	-- messageText:SetHeight(20)
+	-- messageText:SetLayout("fill")
+	-- messageText.text = messageText.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
+	-- messageText.text:SetText("> Str: "..card.strength .. " deck: "..card.deck)
+	-- messageText.text:SetWordWrap(true)
+	-- messageText.text:SetJustifyH("left")
+	-- messageText.text:SetJustifyV("top")
+	-- messageText.text:SetPoint("topleft", 0, 0)
+	-- messageText.text:SetPoint("bottomright")
+	-- DEBUGWINDOW.cardScroller:AddChild(messageText)
+end
+
+local function DEBUGAddMessage(message, sender)
+
+	local messageSender = AceGUI:Create("Label")
+	messageSender:SetRelativeWidth(1)
+	messageSender:SetText("|cFFE6D707Message from "..sender .. "|r")
+	DEBUGWINDOW.mainScroller:AddChild(messageSender)
+	
+	local messageText = AceGUI:Create("Label")
+	messageText:SetRelativeWidth(1)
+	--messageText.label:SetWordWrap(true)
+	messageText:SetText("> "..message)
+	DEBUGWINDOW.mainScroller:AddChild(messageText)
+	
+	-- local messageSender = AceGUI:Create("SimpleGroup")
+	-- messageSender:SetRelativeWidth(1)
+	-- messageSender:SetHeight(10)
+	-- messageSender:SetLayout("fill")
+	-- messageSender.text = messageSender.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
+	-- messageSender.text:SetText("|cFFE6D707Message from "..sender .. "|r")
+	-- messageSender.text:SetJustifyH("left")
+	-- messageSender.text:SetJustifyV("top")
+	-- messageSender.text:SetPoint("topleft", 0, 0)
+	-- messageSender.text:SetPoint("bottomright")
+	-- DEBUGWINDOW.mainScroller:AddChild(messageSender)
+	
+	
+	
+	-- local messageText = AceGUI:Create("SimpleGroup")
+	-- messageText:SetRelativeWidth(1)
+	-- messageText:SetHeight(20)
+	-- messageText:SetLayout("fill")
+	-- messageText.text = messageText.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
+	-- messageText.text:SetText("> "..message)
+	-- messageText.text:SetWordWrap(true)
+	-- messageText.text:SetJustifyH("left")
+	-- messageText.text:SetJustifyV("top")
+	-- messageText.text:SetPoint("topleft", 0, 0)
+	-- messageText.text:SetPoint("bottomright")
+	-- DEBUGWINDOW.mainScroller:AddChild(messageText)
+end
+
+local function DEBUGMessageSuccess(sender)
+	
+end
+
+local function DEBUGPrintMessages()
+	if DEBUGWINDOW.mainScroller == nil then return end
+	
+	DEBUGWINDOW.mainScroller:ReleaseChildren()
+
+	for k , v in ipairs(DEBUGMESSAGES) do
+			DEBUGAddMessage(v.message, v.sender)
+	end
+
+end
+
+local function DEBUGShowMessageContainer(container)
+	container:SetLayout("Fill")
+	DEBUGWINDOW.mainScroller = AceGUI:Create("ScrollFrame")
+	DEBUGWINDOW.mainScroller:SetLayout("Flow")
+	container:AddChild(DEBUGWINDOW.mainScroller)
+	
+	DEBUGPrintMessages()
+end
+
+local function DEBUGShowCards(container)
+	
+	container:SetLayout("Fill")
+	DEBUGWINDOW.cardScroller = AceGUI:Create("ScrollFrame")
+	DEBUGWINDOW.cardScroller:SetLayout("Flow")
+	container:AddChild(DEBUGWINDOW.cardScroller)
+	for k , v in ipairs(GwentAddon.CardList) do
+
+			DEBUGPrintCard(v)
+	end
+end
+
+local function DEBUGSelectTab(container, event, group)
+   container:ReleaseChildren()
+   if group == "messages" then
+     DEBUGShowMessageContainer(container)
+   elseif group == "cards" then
+     DEBUGShowCards(container)
+   end
+end
+
 local function CreateDebug()
  
 	DEBUGWINDOW = AceGUI:Create("Frame")
 	DEBUGWINDOW:SetTitle("Gwent Debug")
 	DEBUGWINDOW:SetLayout("Fill")
 	DEBUGWINDOW:SetWidth(250)
-	DEBUGWINDOW.frame:Hide()
+	--DEBUGWINDOW.frame:Hide()
+
+	DEBUGWINDOW.tabs = AceGUI:Create("TabGroup");
+	DEBUGWINDOW.tabs:SetLayout("Flow")
+	DEBUGWINDOW.tabs:SetTabs({{text = "Messages", value="messages"}, {text = "CardList", value="cards"}})
+	DEBUGWINDOW.tabs:SetCallback("OnGroupSelected", DEBUGSelectTab)
+	DEBUGWINDOW.tabs:SelectTab("messages")
+	DEBUGWINDOW:AddChild(DEBUGWINDOW.tabs)
 	
-	DEBUGWINDOW.mainScroller = AceGUI:Create("ScrollFrame")
-	DEBUGWINDOW.mainScroller:SetLayout("Flow")
-	DEBUGWINDOW:AddChild(DEBUGWINDOW.mainScroller)
+	
+	
+	--DEBUGWINDOW:AddChild(DEBUGWINDOW.mainScroller)
 
 end
-
-local function DEBUGAddMessage(message, sender)
-	local messageSender = AceGUI:Create("SimpleGroup")
-	messageSender:SetRelativeWidth(1)
-	messageSender:SetHeight(10)
-	messageSender:SetLayout("fill")
-	messageSender.text = messageSender.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
-	messageSender.text:SetText("|cFFE6D707Message from "..sender .. "|r")
-	messageSender.text:SetJustifyH("left")
-	messageSender.text:SetJustifyV("top")
-	messageSender.text:SetPoint("topleft", 0, 0)
-	messageSender.text:SetPoint("bottomright")
-	DEBUGWINDOW.mainScroller:AddChild(messageSender)
-	
-	
-	
-	local messageText = AceGUI:Create("SimpleGroup")
-	messageText:SetRelativeWidth(1)
-	messageText:SetHeight(20)
-	messageText:SetLayout("fill")
-	messageText.text = messageText.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
-	messageText.text:SetText("> "..message)
-	messageText.text:SetWordWrap(true)
-	messageText.text:SetJustifyH("left")
-	messageText.text:SetJustifyV("top")
-	messageText.text:SetPoint("topleft", 0, 0)
-	messageText.text:SetPoint("bottomright")
-	DEBUGWINDOW.mainScroller:AddChild(messageText)
-end
-
-local function DEBUGMessageSuccess(sender)
-	local messageSender = AceGUI:Create("SimpleGroup")
-	messageSender:SetRelativeWidth(1)
-	messageSender:SetHeight(10)
-	messageSender:SetLayout("fill")
-	messageSender.text = messageSender.frame:CreateFontString(nil, nil, "GameFontHighlightSmall")
-	messageSender.text:SetText("|cFF00CC0EMessage to "..sender .. " success|r")
-	messageSender.text:SetJustifyH("left")
-	messageSender.text:SetJustifyV("top")
-	messageSender.text:SetPoint("topleft", 0, 0)
-	messageSender.text:SetPoint("bottomright")
-	DEBUGWINDOW.mainScroller:AddChild(messageSender)
-end
-
 
 local L_FPS_LoadFrame = CreateFrame("FRAME", "Gwent_EventFrame"); 
 Gwent_EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
 Gwent_EventFrame:RegisterEvent("CHAT_MSG_ADDON");
 Gwent_EventFrame:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
 
-function Gwent_EventFrame:CHAT_MSG_ADDON(prefix, message, channel, sender)
+function Gwent_EventFrame:CHAT_MSG_ADDON(prefix, m, channel, s)
 	if prefix ~= addonName then
 		return
 	end
 	
 	if message == TEXT_ADDONMSG_RECIEVED then
-		DEBUGMessageSuccess(sender)
+		--DEBUGMessageSuccess(sender)
 		return
 	end
 	
-	DEBUGAddMessage(message, sender)
-	
+	table.insert(DEBUGMESSAGES, {sender = s, message = m})
+
+	DEBUGPrintMessages()
 	
 	-- Send Success Message
-	SendAddonMessage(addonName, TEXT_ADDONMSG_RECIEVED , "whisper" , sender)
+	--SendAddonMessage(addonName, TEXT_ADDONMSG_RECIEVED , "whisper" , s)
 
 	
 end
 
 function Gwent_EventFrame:PLAYER_ENTERING_WORLD(loadedAddon)
 	CreateDebug()
-
 	if not RegisterAddonMessagePrefix(addonName) then
 		print(addonName ..": Could not register prefix.")
 	end
@@ -131,12 +219,14 @@ local function slashcmd(msg, editbox)
 		end
 		
 		SendAddonMessage(addonName, "This is a test message "..name , "whisper" , name)
+		SendAddonMessage(addonName, "What if I type a really long text, will it wrap or not I don't know "..name , "whisper" , name)
 		
-	elseif msg == 'info' then
+	elseif msg == 'create' then
 	
+	GwentAddon:CreateCardsList()
 	
 	elseif msg == 'clear' then
-	
+		DEBUGWINDOW.mainScroller:ReleaseChildren()
 	
 	else
 		--if ( not InterfaceOptionsFramePanelContainer.displayedPanel ) then
